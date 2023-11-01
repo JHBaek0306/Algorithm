d6a761c9538d3df29676ca5cf43f624c4d91c20a
JHBaek0306 <cjddns402@naver.com>
2023-11-01 23:32:10 +0900

    add comment

--- RightBigNumber.py
+++ RightBigNumber.py
@@ -1,14 +1,16 @@
-n = int(input())
-ans = [0] * n
-A = list(map(int, input().split()))
-myStack = []
+# 17298
+n = int(input()) # number of list
+ans = [0] * n # answer list 
+A = list(map(int, input().split())) # original list 
+myStack = [] # using stack to find out target number 
 
+# put the index not the value of the list 
 for i in range(n):
-    while myStack and A[myStack[-1]] < A[i]:
-        ans[myStack.pop()] = A[i]
+    while myStack and A[myStack[-1]] < A[i]: # number exist in Stack and last index number is smaller than original list number 
+        ans[myStack.pop()] = A[i] 
     myStack.append(i)
 
-while myStack:
+while myStack: # fill -1 remainder index 
     ans[myStack.pop()] = -1
 
 result = ''
